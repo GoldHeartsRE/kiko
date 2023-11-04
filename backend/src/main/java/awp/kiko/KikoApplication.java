143423f98ce3d8eda7863c086ec643f3fb4919e9
burakde2001 <burakde2001@gmail.com>
2023-11-04 12:54:51 +0100

    Revert "Test Branch Verbindung"
    This reverts commit 4a027d57bc1a60ab9e1aada8136b8ebb87e47583.

--- backend/src/main/java/awp/kiko/KikoApplication.java
+++ backend/src/main/java/awp/kiko/KikoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KikoApplication {
 
-	//Test
 	public static void main(String[] args) {
 		SpringApplication.run(KikoApplication.class, args);
 	}
