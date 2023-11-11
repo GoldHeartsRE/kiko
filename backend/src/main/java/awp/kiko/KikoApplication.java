45a10ad71207884bfb43c95d8ac9a3fa678d07a1
fued1011-2 <fuchs_edgar@web.de>
2023-11-11 12:02:44 +0100

    Added postman Collection Login. Added CORS.

--- backend/src/main/java/awp/kiko/KikoApplication.java
+++ backend/src/main/java/awp/kiko/KikoApplication.java
@@ -2,13 +2,27 @@ package awp.kiko;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication()
+@CrossOrigin
 public class KikoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KikoApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:19006");
+			}
+		};
+	}
 }
