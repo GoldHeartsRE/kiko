9b34b4a440e81c170df6bb69d12748b393d421c8
GoldHeartsRE <dennis.huelsken@outlook.de>
2023-11-11 13:25:46 +0100

    HTTP API Login/Logout

--- ui/KiKo/App.js
+++ ui/KiKo/App.js
@@ -42,7 +42,7 @@ export default function App() {
     <Provider theme={theme}>
       <NavigationContainer>
         <Stack.Navigator
-          initialRouteName="AnsprechpartnerScreen"
+          initialRouteName="StartScreen"
           screenOptions={{
             headerShown: false,
           }}
