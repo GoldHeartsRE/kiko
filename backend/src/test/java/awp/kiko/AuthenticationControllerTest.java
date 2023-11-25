dec2bc0f480ba2ba21e54e1fb4ec06d8ce1d05bb
Luca <lucaleitzbach@yahoo.de>
2023-11-25 13:03:57 +0100

    Tests angepasst

--- backend/src/test/java/awp/kiko/AuthenticationControllerTest.java
+++ backend/src/test/java/awp/kiko/AuthenticationControllerTest.java
@@ -1,150 +1,143 @@
-// package awp.kiko;
+package awp.kiko;
 
-// import static org.assertj.core.api.Assertions.assertThat;
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-// import org.aspectj.lang.annotation.Before;
-// import org.junit.jupiter.api.Disabled;
-// import org.junit.jupiter.api.Test;
-// import org.mockito.Mock;
-// import org.mockito.Mockito;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-// import org.springframework.boot.test.mock.mockito.MockBean;
-// import org.springframework.boot.test.web.server.LocalServerPort;
-// import org.springframework.http.MediaType;
-// import org.springframework.security.test.context.support.WithMockUser;
-// import org.springframework.test.web.servlet.MockMvc;
-// import org.springframework.test.web.servlet.MvcResult;
-// import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-// import com.fasterxml.jackson.databind.ObjectMapper;
-// import com.jayway.jsonpath.JsonPath;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
 
-// import static org.hamcrest.Matchers.containsString;
-// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-// import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-// import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-// import awp.kiko.dao.request.SignUpRequest;
-// import awp.kiko.dao.request.SigninRequest;
-// import awp.kiko.dao.response.JwtAuthenticationResponse;
-// import awp.kiko.entity.Role;
-// import awp.kiko.entity.User;
-// import awp.kiko.rest.AuthenticationController;
-// import awp.kiko.security.AuthenticationService;
-// import awp.kiko.security.impl.JwtServiceImpl;
-// import awp.kiko.validation.ObjectValidator;
+import awp.kiko.dao.request.SignUpRequest;
+import awp.kiko.dao.request.SigninRequest;
+import awp.kiko.dao.response.JwtAuthenticationResponse;
+import awp.kiko.entity.Partner;
+import awp.kiko.entity.Role;
+import awp.kiko.entity.User;
+import awp.kiko.rest.AuthenticationController;
+import awp.kiko.security.AuthenticationService;
+import awp.kiko.security.JwtService;
+import awp.kiko.rest.AuthorizationController;
 
-// import awp.kiko.rest.AuthorizationController;
-
-// @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-// @AutoConfigureMockMvc(addFilters = false)
-// public class AuthenticationControllerTest {
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc(addFilters = false)
+public class AuthenticationControllerTest {
     
-// private SignUpRequest signupRequest;
-// private SigninRequest signinRequest;
+private SignUpRequest signupRequest;
+private SigninRequest signinRequest;
 
-//     @Autowired
-//     private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-//     @MockBean
-//     private AuthenticationService authenticationService;
+    @Autowired
+    private AuthenticationService authenticationService;
 
-//     @Autowired
-//     private ObjectMapper objectMapper;
+    @Autowired
+    JwtService jwtService;
 
-//     @Autowired
-//     JwtServiceImpl jwtServiceImpl;
+    @Test
+    void postSignup200() throws Exception {
+        signupRequest = this.getSignUpRequestUser("kiko@test.de", "abc");
 
-//     @Test
-//     void postSignup200() throws Exception {
-//         signupRequest = this.getSignUpRequestUser("kiko@test.de", "abc");
+        MvcResult mvcResult = mockMvc.perform(post("/api/v1/auth/signup")
+            .content(asJsonString(signupRequest))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andReturn();
 
-//         MvcResult mvcResult = mockMvc.perform(post("/api/v1/auth/signup")
-//             .content(asJsonString(signupRequest))
-//             .contentType(MediaType.APPLICATION_JSON)
-//             .accept(MediaType.APPLICATION_JSON))
-//             .andReturn();
+        String token =JsonPath.read(mvcResult.getResponse().getContentAsString(), "$.token");
 
-//         String token =JsonPath.read(mvcResult.getResponse().getContentAsString(), "$.token");
+        assertEquals(200, mvcResult.getResponse().getStatus());
+        assertEquals("kiko@test.de", jwtService.extractUserName(token));
+    }
 
-//         assertEquals(200, mvcResult.getResponse().getStatus());
-//         assertEquals("kiko@test.de", jwtServiceImpl.extractUserName(token));
-//     }
+    // @Test
+    // @WithMockUser
+    // void postSignIn200() throws Exception {
 
-//     @Test
-//     @WithMockUser
-//     void postSignIn200() throws Exception {
+    //     // Mockito.when(authenticationService.signin(
+    //     //     signinRequest
+    //     // )).thenReturn(jwtServiceImpl.generateToken(createUser(1, "test@kiko.de", "abc")));
+    //     // signinRequest = SigninRequest.builder()
+    //     //     .email("test@kiko.de")
+    //     //     .password("abc")
+    //     //     .build();
 
-//         // Mockito.when(authenticationService.signin(
-//         //     signinRequest
-//         // )).thenReturn(jwtServiceImpl.generateToken(createUser(1, "test@kiko.de", "abc")));
-//         // signinRequest = SigninRequest.builder()
-//         //     .email("test@kiko.de")
-//         //     .password("abc")
-//         //     .build();
+    //     JwtAuthenticationResponse jwtarp = JwtAuthenticationResponse.builder().token(jwtServiceImpl.generateToken(createUser(1, "kiko@test.de", "abc"))).build();
 
-//         JwtAuthenticationResponse jwtarp = JwtAuthenticationResponse.builder().token(jwtServiceImpl.generateToken(createUser(1, "kiko@test.de", "abc"))).build();
-
-//         signinRequest = this.getSigninRequest("kiko@test.de", "abc");
+    //     signinRequest = this.getSigninRequest("kiko@test.de", "abc");
         
-//         Mockito.when(authenticationService.signin(signinRequest)).thenReturn(jwtarp);
+    //     Mockito.when(authenticationService.signin(signinRequest)).thenReturn(jwtarp);
 
-//         System.out.println(jwtarp.toString());
+    //     System.out.println(jwtarp.toString());
 
-//         mockMvc.perform(post("/api/v1/auth/signin").content(asJsonString(signinRequest))
-//         .contentType(MediaType.APPLICATION_JSON)
-//         .accept(MediaType.APPLICATION_JSON))
-//         .andExpect(status().isOk());
-//     }
+    //     mockMvc.perform(post("/api/v1/auth/signin").content(asJsonString(signinRequest))
+    //     .contentType(MediaType.APPLICATION_JSON)
+    //     .accept(MediaType.APPLICATION_JSON))
+    //     .andExpect(status().isOk());
+    // }
         
-// public static String asJsonString(final Object obj) {
-//     try {
-//         return new ObjectMapper().writeValueAsString(obj);
-//     } catch (Exception e) {
-//         throw new RuntimeException(e);
-//     }
-// }
+public static String asJsonString(final Object obj) {
+    try {
+        return new ObjectMapper().writeValueAsString(obj);
+    } catch (Exception e) {
+        throw new RuntimeException(e);
+    }
+}
 
-// private SignUpRequest getSignUpRequestUser(String email, String password) {
+private SignUpRequest getSignUpRequestUser(String email, String password) {
 
-//     SignUpRequest signupRequest = SignUpRequest.builder()
-//         .email(email)
-//         .password(password)
-//         .role(Role.USER)
-//         .build();
+    SignUpRequest signupRequest = SignUpRequest.builder()
+        .email(email)
+        .password(password)
+        .role(Role.PARTNER)
+        .build();
 
-//     return signupRequest;
-// }
+    return signupRequest;
+}
 
-// private SigninRequest getSigninRequest(String email, String password) {
-//     SigninRequest signinRequest = SigninRequest.builder()
-//         .email(email)
-//         .password(password)
-//         .build();
-//     return signinRequest;
-// }
+private SigninRequest getSigninRequest(String email, String password) {
+    SigninRequest signinRequest = SigninRequest.builder()
+        .email(email)
+        .password(password)
+        .build();
+    return signinRequest;
+}
 
-// private User createUser(int id, String email, String password) {
-//     User user = User.builder()
-//         .id(id)
-//         .email(email)
-//         .password(password)
-//         .build();
-//     return user;
-// }
-    
-// }
+private Partner createUser(int id, String email, String password) {
+    Partner partner = (Partner.builder().id(1).email(email).password(password).role(Role.PARTNER)).build();
+
+    return partner;
+}
+
+}
--- backend/src/test/java/awp/kiko/AuthenticationServiceTest.java
+++ backend/src/test/java/awp/kiko/AuthenticationServiceTest.java
@@ -4,50 +4,65 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.exceptions.base.MockitoException;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import awp.kiko.dao.request.SignUpRequest;
+import awp.kiko.dao.request.SigninRequest;
+import awp.kiko.dao.response.IdJwtAuthenticationResponse;
 import awp.kiko.entity.Partner;
 import awp.kiko.entity.Role;
 import awp.kiko.entity.User;
 import awp.kiko.repository.KitaRepository;
 import awp.kiko.repository.PartnerRepository;
 import awp.kiko.repository.UserRepository;
+import awp.kiko.security.AuthenticationService;
 import awp.kiko.security.JwtService;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class AuthenticationServiceTest {
     
-    @MockBean
+    private AuthenticationService authenticationService;
+
+    @Mock
     private UserRepository userRepository;
 
-    @MockBean
+    @Mock
     private PartnerRepository partnerRepository;
 
-    @MockBean
+    @Mock
     private KitaRepository kitaRepository;
 
-    @MockBean
-    private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder() ;
 
-    private JwtService jwtService = new JwtService();
+    private final JwtService jwtServiceMock = new JwtService();
 
-    @MockBean
+    @Mock
     private AuthenticationManager authenticationManager;
 
     @Test
     void signup() {
-        User kikoUser;
+
+        authenticationService = new AuthenticationService(userRepository, partnerRepository, kitaRepository, passwordEncoder, jwtServiceMock, authenticationManager);
+        SignUpRequest signUpRequest = getSignUpRequestPartnerMock("test@kiko.de", "abc");
+
         final Partner partnerMock = createPartnerMock(1, "test@kiko.de", "abc");
         when(partnerRepository.save(partnerMock)).thenReturn(partnerMock);
-        
+
+        IdJwtAuthenticationResponse response = authenticationService.signup(signUpRequest);
+
+        System.out.println(response.toString());
 
     }
-
 
     private Partner createPartnerMock(int id, String email, String password) {
         Partner partner = Partner
