2cae224c6d6b11427a05ff972bdac273e801cadf
fued1011-2 <fuchs_edgar@web.de>
2023-12-06 16:58:59 +0100

    Fehler behoben

--- backend/src/main/java/awp/kiko/entity/Adresse.java
+++ backend/src/main/java/awp/kiko/entity/Adresse.java
@@ -25,37 +25,12 @@ public class Adresse {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer adresse_id;
 
-    @Digits(fraction = 0, integer = 5, message = ErrorMessages.PLZ_UNGUELTIG)
     private Integer plz;
 
-    @Pattern.List({
-            @Pattern(regexp = "\\S.*", message = ErrorMessages.PATTERN_FEHLER_1),
-            @Pattern(regexp = ".*\\S", message = ErrorMessages.PATTERN_FEHLER_2),
-            @Pattern(regexp = "((?!\\s\\s).)*", message = ErrorMessages.PATTERN_FEHLER_3a),
-            @Pattern(regexp = "((?![A-Z][A-Z]).)*", message = ErrorMessages.PATTERN_FEHLER_4),
-            @Pattern(regexp = "[^a-z].*$", message = ErrorMessages.PATTERN_FEHLER_5),
-            @Pattern(regexp = "[A-Z][a-z]+\\s?-?", message = ErrorMessages.PATTERN_FEHLER_6)
-    })
     private String ort;
 
-    //TODO: Pattern bezüglich '.' (Str.)
-    @Pattern.List({
-            @Pattern(regexp = "\\S.*", message = ErrorMessages.PATTERN_FEHLER_1),
-            @Pattern(regexp = ".*\\S", message = ErrorMessages.PATTERN_FEHLER_2),
-            @Pattern(regexp = "((?!\\s\\s).)*", message = ErrorMessages.PATTERN_FEHLER_3a),
-            @Pattern(regexp = "((?![A-Z][A-Z]).)*", message = ErrorMessages.PATTERN_FEHLER_4),
-            @Pattern(regexp = "[^a-z].*", message = ErrorMessages.PATTERN_FEHLER_5),
-            @Pattern(regexp = "[A-Za-z]+\\s?-?")
-    })
     private String strasse;
 
-    @Pattern.List({
-            @Pattern(regexp = "\\S.*", message = ErrorMessages.PATTERN_FEHLER_1),
-            @Pattern(regexp = ".*\\S", message = ErrorMessages.PATTERN_FEHLER_2),
-            @Pattern(regexp = "((?!\\s).)*", message = ErrorMessages.PATTERN_FEHLER_3b),
-            @Pattern(regexp = "((?![A-Za-z][A-Za-z]).)*", message = ErrorMessages.PATTERN_FEHLER_4),
-            @Pattern(regexp = "[0-9]+[A-Za-z]?", message = ErrorMessages.HAUSNR_UNGUELTIG)
-    })
     private String nr;
 
     public String strasseMitNr() {
