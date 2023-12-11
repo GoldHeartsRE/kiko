2027833cf49498b20cd8fedaa71538f83c9b544c
dominikdanielgonsior@gmail.com <dominikdanielgonsior@gmail.com>
2023-12-11 01:35:08 +0100

    Angebote Kita touchable

--- ui/KiKo/src/components/KitaMarktplaceComponents/AngebotKitaView.js
+++ ui/KiKo/src/components/KitaMarktplaceComponents/AngebotKitaView.js
@@ -3,22 +3,29 @@ import { StyleSheet, View } from 'react-native'
 import { Paragraph, Text, Card } from 'react-native-paper'
 
 export default function AngebotKitaView({ image, kurstitel, alterVon, alterBis, kindervon, kinderBis, wochentag, dauer, kosten}) {
+
+    const handlePress = () => {
+        // zu Angebot gehen
+      };   
+
   return (
     <View>
         {/* Profilbild hier */}
-        <Card>
-            <Card.Content>
-                <Text variant="titleLarge">{kurstitel}</Text>
-                <Text variant="bodyMedium">Altersgruppe: {alterVon} - {alterBis}</Text>
-                <Text variant="bodyMedium">Gruppengröße: {kindervon} - {kinderBis}</Text>
-                <Text variant="bodyMedium">Wochentag: {wochentag}</Text>
-                <Text variant="bodyMedium">Dauer: {dauer}</Text>
-                <Text variant="bodyMedium">Kosten: {kosten}</Text>
-            </Card.Content>
-            <Card.Actions>
-                {/* hier BUtton um anzuzeigen falls wir die Card nicht touchable machen können */}
-            </Card.Actions>
-        </Card>
+        <TouchableRipple onPress={handlePress} rippleColor="rgba(0, 0, 0, 0.1)">
+            <Card>
+                <Card.Content>
+                    <Text variant="titleLarge">{kurstitel}</Text>
+                    <Text variant="bodyMedium">Altersgruppe: {alterVon} - {alterBis}</Text>
+                    <Text variant="bodyMedium">Gruppengröße: {kindervon} - {kinderBis}</Text>
+                    <Text variant="bodyMedium">Wochentag: {wochentag}</Text>
+                    <Text variant="bodyMedium">Dauer: {dauer}</Text>
+                    <Text variant="bodyMedium">Kosten: {kosten}</Text>
+                </Card.Content>
+                <Card.Actions>
+                    {/* hier BUtton um anzuzeigen falls wir die Card nicht touchable machen können */}
+                </Card.Actions>
+            </Card>
+        </TouchableRipple>
     </View>
   )
 }
