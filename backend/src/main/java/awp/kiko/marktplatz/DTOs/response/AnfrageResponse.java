8cdf215239aa3d7270fc4e6b4cc4f646d7eaa01b
Luca <lucaleitzbach@yahoo.de>
2024-01-02 18:16:25 +0100

    AnfrageResponse erweitert um alle zusätzlichen Informationen

--- backend/src/main/java/awp/kiko/marktplatz/DTOs/response/AnfrageResponse.java
+++ backend/src/main/java/awp/kiko/marktplatz/DTOs/response/AnfrageResponse.java
@@ -5,23 +5,54 @@ import awp.kiko.marktplatz.entity.AnfrageStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 public class AnfrageResponse {
-    private Integer id;
+    private Integer anfrageId;
+
+    private Integer kitaId;
+
+    private Integer partnerId;
+
+    private Integer angebotId;
+
+    private LocalDateTime erstelltAm;
+
+    private LocalDateTime geaendertAm;
 
     private AnfrageStatus status;
+
+
 
     public static List<AnfrageResponse> anfragenToResponse(List<Anfrage> anfragen) {
         List<AnfrageResponse> anfrageResponses = new ArrayList<>();
 
         for (Anfrage anfrage : anfragen) {
-            anfrageResponses.add(new AnfrageResponse(anfrage.getId(), anfrage.getStatus()));
+            anfrageResponses.add(new AnfrageResponse(anfrage.getId(), 
+                                                        anfrage.getKita().getUser_id(), 
+                                                        anfrage.getPartner().getUser_id(), 
+                                                        anfrage.getAngebot().getId(), 
+                                                        anfrage.getErstelltAm(), 
+                                                        anfrage.getGeaendertAm(), 
+                                                        anfrage.getStatus()));
         }
 
         return anfrageResponses;
+    }
+
+    public static AnfrageResponse anfrageToResponse(Anfrage anfrage) {
+        return new AnfrageResponse(anfrage.getId(), 
+                                    anfrage.getKita().getUser_id(), 
+                                    anfrage.getPartner().getUser_id(), 
+                                    anfrage.getAngebot().getId(), 
+                                    anfrage.getErstelltAm(), 
+                                    anfrage.getGeaendertAm(), 
+                                    anfrage.getStatus());
+
+
     }
 }
